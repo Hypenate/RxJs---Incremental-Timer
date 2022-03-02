a083965729d8d0ea9150219fef626dd2cbb8fb03
Hypenate <ben.beckers@gmail.com>
2022-03-02 09:30:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# rxjs-nkvbij
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/rxjs-nkvbij)
---
+++ index.ts
@@ -0,0 +1,25 @@
+import './style.css';
+
+import { of, interval } from 'rxjs';
+import { concatMap, delay, take } from 'rxjs/operators';
+
+function calculateDelay(iteration, initialInterval) {
+  const delay = iteration * initialInterval + 1000;
+  console.log('delay: ' + delay);
+
+  return delay;
+}
+
+const source = interval(2000);
+const spread = source.pipe(
+  concatMap((value, index) => {
+    return of(value).pipe(delay(calculateDelay(index, 2000)));
+  }),
+  take(10) // To finizalize it
+);
+
+spread.subscribe({
+  complete() {
+    console.log('done');
+  },
+});
